9a2ef05d5a2d7c6eceb9a30a46a89d8ee2e2afc6
Kalin Ramsey <kalinramsey@gmail.com>
2021-03-08 05:03:41 -0700

    added mobile nav hamburger menu functionality

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+// hamburger menu toggle
+$(function () {
+	const menu_btn = document.querySelector(".hamburger");
+	const mobile_nav = document.querySelector("#mobile-nav");
+	let nav_link = document.querySelectorAll(".nav-link");
+
+	nav_link.forEach((link) => {
+		link.addEventListener("click", function () {
+			menu_btn.classList.toggle("is-active");
+			mobile_nav.classList.toggle("is-active");
+		});
+	});
+	// nav_link.addEventListener("click", function () {
+	// 	menu_btn.classList.toggle("is-active");
+	// 	mobile_nav.classList.toggle("is-active");
+	// });
+
+	menu_btn.addEventListener("click", function () {
+		menu_btn.classList.toggle("is-active");
+		mobile_nav.classList.toggle("is-active");
+	});
+});
+
 // mgGlitch data
 $(function () {
 	$(".glitch-img").mgGlitch({
@@ -33,6 +56,66 @@ setInterval(function () {
 		glitch[i].style.backgroundPosition = Math.floor(Math.random() * 100) + "px";
 	}
 }, Math.random() * 200);
+
+// Typewriter effect
+var TxtType = function (el, toRotate, period) {
+	this.toRotate = toRotate;
+	this.el = el;
+	this.loopNum = 0;
+	this.period = parseInt(period, 10) || 2000;
+	this.txt = "";
+	this.tick();
+	this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function () {
+	var i = this.loopNum % this.toRotate.length;
+	var fullTxt = this.toRotate[i];
+
+	if (this.isDeleting) {
+		this.txt = fullTxt.substring(0, this.txt.length - 1);
+	} else {
+		this.txt = fullTxt.substring(0, this.txt.length + 1);
+	}
+
+	this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
+
+	var that = this;
+	var delta = 200 - Math.random() * 100;
+
+	if (this.isDeleting) {
+		delta /= 2;
+	}
+
+	if (!this.isDeleting && this.txt === fullTxt) {
+		delta = this.period;
+		this.isDeleting = true;
+	} else if (this.isDeleting && this.txt === "") {
+		this.isDeleting = false;
+		this.loopNum++;
+		delta = 500;
+	}
+
+	setTimeout(function () {
+		that.tick();
+	}, delta);
+};
+
+window.onload = function () {
+	var elements = document.getElementsByClassName("typewrite");
+	for (var i = 0; i < elements.length; i++) {
+		var toRotate = elements[i].getAttribute("data-type");
+		var period = elements[i].getAttribute("data-period");
+		if (toRotate) {
+			new TxtType(elements[i], JSON.parse(toRotate), period);
+		}
+	}
+	// INJECT CSS
+	var css = document.createElement("style");
+	css.type = "text/css";
+	css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #ffffff)}";
+	document.body.appendChild(css);
+};
 
 // "Good Morning/Day/Afternoon/Evening" based on local machine time
 let timeText = document.getElementById("timeText");
